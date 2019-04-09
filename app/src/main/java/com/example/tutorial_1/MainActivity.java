7172a6b5c661c36c712891af3e75dd7d95d5661c
cmich007@fiu.edu <cmich007@fiu.edu>
2019-04-08 21:14:23 -0400

    Adding and displaying answer code

--- app/src/main/java/com/example/tutorial_1/MainActivity.java
+++ app/src/main/java/com/example/tutorial_1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tutorial_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button addButton = (Button) findViewById(R.id.AddButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextview = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                resultTextview.setText(result + "");
+            }
+        });
     }
 }
